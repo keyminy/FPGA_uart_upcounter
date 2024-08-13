51f71dba2ce7fc3c3044d24b55084781756bdbbd
keyminy <imanaifs@gmail.com>
2024-08-13 16:00:05 +0900

    teacher 구조 수정

--- sources_1/imports/new/top_upcnt_btn_FSM.v
+++ sources_1/imports/new/top_upcnt_btn_FSM.v
@@ -3,12 +3,15 @@
 module top_upcnt_btn_FSM(
     input clk,
     input reset,
-    input [7:0] i_rx_data,
-    input i_rx_done,
+    // input [7:0] i_rx_data,
+    // input i_rx_done,
     input btnr,
     input btnu,
     output [3:0] fndCom,
-    output [7:0] fndFont
+    output [7:0] fndFont,
+    // uart
+    input rx,
+    output tx
     );
 
     wire [13:0] w_i_digit;
@@ -16,18 +19,8 @@ module top_upcnt_btn_FSM(
     wire w_clr_on;
     wire w_btn_run_stop,w_btn_clear;
 
-    debounce u_btn_runstop(
-    .clk(clk),
-    .reset(reset),
-    .i_btn(btnr),
-    .o_btn(w_btn_run_stop)
-    );
-    debounce u_btn_clear(
-    .clk(clk),
-    .reset(reset),
-    .i_btn(btnu),
-    . o_btn(w_btn_clear)
-    );
+    wire w_rx_done;
+    wire [7:0] w_rx_data;
 
     uart u_uart_idk01(
         // globla signal
@@ -43,12 +36,25 @@ module top_upcnt_btn_FSM(
         .o_rx_data(w_rx_data),
         .o_rx_done(w_rx_done)
     );
-    
+
+    debounce u_btn_runstop(
+    .clk(clk),
+    .reset(reset),
+    .i_btn(btnr),
+    .o_btn(w_btn_run_stop)
+    );
+    debounce u_btn_clear(
+    .clk(clk),
+    .reset(reset),
+    .i_btn(btnu),
+    . o_btn(w_btn_clear)
+    );
+
     fsm_btn U_fsm_wawtch(
         .clk(clk),
         .reset(reset),
-        .i_rx_data(i_rx_data),
-        .i_rx_done(i_rx_done),
+        .i_rx_data(w_rx_data),
+        .i_rx_done(w_rx_done),
         .btnr(w_btn_run_stop),
         .btnu(w_btn_clear),
         .o_run_on(w_run_on),
--- sources_1/new/top_uart_fnd.v
+++ sources_1/new/top_uart_fnd.v
@@ -7,39 +7,39 @@ module top_uart_fnd(
     input btnr,
     input btnu,
     output [3:0] fndCom,
-    output [7:0] fndFont,
+    output [7:0] fndFont
     // uart
-    input rx,
-    output tx
+    // input rx,
+    // output tx
     );
 
-    wire w_rx_done;
-    wire [7:0] w_rx_data;
+    // wire w_rx_done;
+    // wire [7:0] w_rx_data;
 
-    top_upcnt_btn_FSM u_btn_fsm (
-        .clk(clk),
-        .reset(reset),
-        .i_rx_data(w_rx_data),
-        .i_rx_done(w_rx_done),
-        .btnr(btnr),
-        .btnu(btnu),
-        .fndCom(fndCom),
-        .fndFont(fndFont)
-    );
+//    top_upcnt_btn_FSM u_btn_fsm (
+//        .clk(clk),
+//        .reset(reset),
+//        .i_rx_data(w_rx_data),
+//        .i_rx_done(w_rx_done),
+//        .btnr(btnr),
+//        .btnu(btnu),
+//        .fndCom(fndCom),
+//        .fndFont(fndFont)
+//    );
 
-    uart u_uart_idk01(
-        // globla signal
-        .clk(clk),
-        .reset(reset),
-        // tx signal
-        .start(w_rx_done),
-        .tx_data(w_rx_data),
-        .o_txd(tx),
-        .o_tx_done(), // no connect
-        // rx signal
-        .rx(rx),
-        .o_rx_data(w_rx_data),
-        .o_rx_done(w_rx_done)
-    );
+    // uart u_uart_idk01(
+    //     // globla signal
+    //     .clk(clk),
+    //     .reset(reset),
+    //     // tx signal
+    //     .start(w_rx_done),
+    //     .tx_data(w_rx_data),
+    //     .o_txd(tx),
+    //     .o_tx_done(), // no connect
+    //     // rx signal
+    //     .rx(rx),
+    //     .o_rx_data(w_rx_data),
+    //     .o_rx_done(w_rx_done)
+    // );
     
 endmodule
